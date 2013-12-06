0d48fa9b00ba736bae9822269c574f429034d157
Akira Ishii <exilias774@gmail.com>
2013-12-06 15:53:27 +0900

    ブロックが落ちるようになった

--- field.c
+++ field.c
@@ -1,7 +1,16 @@
 #include "field.h"
 
+#define FPS					60
+#define UPDATE_INTERVAL		40
+#define ADD_BLOCK_INTERVAL	180
 
 int field[FIELD_SIZE_WIDTH][FIELD_SIZE_HEIGHT];	
+
+
+int getRandomBlock()
+{
+	return rand() % 3 + 1;
+}
 
 void fieldInit()
 {
@@ -15,12 +24,35 @@ void fieldInit()
 
 	for (i = 0; i < FIELD_SIZE_WIDTH; i++) {
 		for (j = FIELD_SIZE_HEIGHT - 2; j < FIELD_SIZE_HEIGHT; j++) {
-			field[i][j] = rand() % 3 + 1;
+			field[i][j] = getRandomBlock();
 		}
 	}
 }
 
 void fieldFunc()
 {
-	
+	static int count = 0;
+
+	if (count % UPDATE_INTERVAL == 0) {
+		int i, j;
+
+		// ブロックを下に落とす
+		for (i = 0; i < FIELD_SIZE_WIDTH; i++) {
+			for (j = FIELD_SIZE_HEIGHT-2; j >= 0; j--) {
+				if (field[i][j] != FIELD_KIND_NONE) {
+					if (field[i][j+1] == FIELD_KIND_NONE) {
+						field[i][j+1] = field[i][j];
+						field[i][j] = FIELD_KIND_NONE;
+					}
+				}
+			}
+		}
+	}
+
+	if (count % ADD_BLOCK_INTERVAL == 0) {
+		// 新たにブロックを生成する
+		field[rand()%FIELD_SIZE_WIDTH][0] = getRandomBlock();
+	}
+
+	count++;
 }
