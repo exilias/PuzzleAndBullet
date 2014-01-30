e1aef2af883db02efb6d4d4676f447769d240f0a
Akira Ishii <exilias774@gmail.com>
2014-01-31 02:24:54 +0900

    背景の雲を3つに変更

--- game_bg.c
+++ game_bg.c
@@ -6,40 +6,53 @@
 #include "math.h"
 
 
-#define GAME_BG_OBJECT_MAX	2
+#define GAME_BG_CLOUD_OBJECT_MAX	3
 
-typedef struct gameBgObject
+typedef struct gameBgCloudObject
 {
 	int x, y;
 	int dx, dy;
 	int interval;
 	int counter;
 	int fileId;
-}GameBgObject;
+	float scale;
+}GameBgCloudObject;
 
 
-GameBgObject gameBgObject[GAME_BG_OBJECT_MAX];
+GameBgCloudObject gameBgCloudObject[GAME_BG_CLOUD_OBJECT_MAX];
 
 
 void gameBgInit()
 {
-	// 雲（大）
-	gameBgObject[0].x = 100;
-	gameBgObject[0].y = 20;
-	gameBgObject[0].dx = 1;
-	gameBgObject[0].dy = 0;
-	gameBgObject[0].interval = 3;
-	gameBgObject[0].counter = 0;
-	gameBgObject[0].fileId = AG_CG_GAME_BG_CLOUD_BIG;
+	// 雲（小）
+	gameBgCloudObject[0].x = 400;
+	gameBgCloudObject[0].y = 200;
+	gameBgCloudObject[0].dx = 1;
+	gameBgCloudObject[0].dy = 0;
+	gameBgCloudObject[0].interval = 15;
+	gameBgCloudObject[0].counter = 0;
+	gameBgCloudObject[0].scale = 1.0f;
+	gameBgCloudObject[0].fileId = AG_CG_GAME_BG_CLOUD_SMALL;
 
 	// 雲（小）
-	gameBgObject[1].x = 400;
-	gameBgObject[1].y = 200;
-	gameBgObject[1].dx = 1;
-	gameBgObject[1].dy = 0;
-	gameBgObject[1].interval = 10;
-	gameBgObject[1].counter = 0;
-	gameBgObject[1].fileId = AG_CG_GAME_BG_CLOUD_SMALL;
+	gameBgCloudObject[1].x = 600;
+	gameBgCloudObject[1].y = 150;
+	gameBgCloudObject[1].dx = 1;
+	gameBgCloudObject[1].dy = 0;
+	gameBgCloudObject[1].interval = 10;
+	gameBgCloudObject[1].counter = 0;
+	gameBgCloudObject[1].scale = 1.3f;
+	gameBgCloudObject[1].fileId = AG_CG_GAME_BG_CLOUD_SMALL;
+
+	// 雲（大）
+	gameBgCloudObject[2].x = 100;
+	gameBgCloudObject[2].y = 20;
+	gameBgCloudObject[2].dx = 1;
+	gameBgCloudObject[2].dy = 0;
+	gameBgCloudObject[2].interval = 3;
+	gameBgCloudObject[2].counter = 0;
+	gameBgCloudObject[2].scale = 1.0f;
+	gameBgCloudObject[2].fileId = AG_CG_GAME_BG_CLOUD_BIG;
 }
 
 
@@ -47,13 +60,13 @@ void gameBgFunc()
 {
 	int i;
 
-	for (i = 0; i < GAME_BG_OBJECT_MAX; i++) {
-		if (gameBgObject[i].counter >= gameBgObject[i].interval) {
-			gameBgObject[i].x += gameBgObject[i].dx;
-			gameBgObject[i].y += gameBgObject[i].dy;
-			gameBgObject[i].counter = 0;
+	for (i = 0; i < GAME_BG_CLOUD_OBJECT_MAX; i++) {
+		if (gameBgCloudObject[i].counter >= gameBgCloudObject[i].interval) {
+			gameBgCloudObject[i].x += gameBgCloudObject[i].dx;
+			gameBgCloudObject[i].y += gameBgCloudObject[i].dy;
+			gameBgCloudObject[i].counter = 0;
 		}
-		gameBgObject[i].counter++;
+		gameBgCloudObject[i].counter++;
 	}
 }
 
@@ -72,13 +85,13 @@ void gameBgDraw(void* DBuf)
 	agDrawSETDBMODE( _DBuf, 0xff, 0, 2, 1 );
 	agDrawSPRITE(_DBuf, TRUE, 0, 0, x4(w), x4(h));
 
-	for (i = 0; i < GAME_BG_OBJECT_MAX; i++) {
-		ageTransferAAC( _DBuf, gameBgObject[i].fileId, 0, &w, &h );
+	for (i = 0; i < GAME_BG_CLOUD_OBJECT_MAX; i++) {
+		ageTransferAAC( _DBuf, gameBgCloudObject[i].fileId, 0, &w, &h );
 		agDrawSETDBMODE( _DBuf, 0xff, 0, 2, 1 );
-		agDrawSPRITE(_DBuf, TRUE, x4(gameBgObject[i].x), x4(gameBgObject[i].y), x4(gameBgObject[i].x + w), x4(gameBgObject[i].y + h));
+		agDrawSPRITE(_DBuf, TRUE, x4(gameBgCloudObject[i].x), x4(gameBgCloudObject[i].y), x4(gameBgCloudObject[i].x + (int)(w * gameBgCloudObject[i].scale)), x4(gameBgCloudObject[i].y + (int)(h * gameBgCloudObject[i].scale)));
 
-		if (gameBgObject[i].x >= 1024) {
-				gameBgObject[i].x = -w;
+		if (gameBgCloudObject[i].x >= 1024) {
+				gameBgCloudObject[i].x = -w;
 			}
 	}
 }
